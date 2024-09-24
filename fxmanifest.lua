b3225378b5e51fca1c07bcfb82b64f5c46cdaa26
Flamtky <niklas.schumann+github@proton.me>
2024-09-24 15:43:38 +0200

    removed debug print | add fail safe on reload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 author 'Flamtky'
 description 'Respawn System for nearby respawning, like in GTA Online'
 repository 'https://github.com/flamtky/fivem-respawn-nearby'
-version '1.3.0'
+version '1.3.1'
 
 resource_type 'gametype' { name = 'Freeroam' }
 
