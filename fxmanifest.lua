cdbc8d5242b451acf2c7ed7c6fe5512f107f00bf
Flamtky <niklas.schumann+github@proton.me>
2024-09-28 17:50:23 +0200

    remove resource_type

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,6 +6,4 @@ description 'Respawn System for nearby respawning, like in GTA Online'
 repository 'https://github.com/flamtky/fivem-respawn-nearby'
 version '1.3.1'
 
-resource_type 'gametype' { name = 'Freeroam' }
-
 client_script 'client.lua'
